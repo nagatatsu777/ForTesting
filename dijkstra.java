35cd51136c5ede37e0baa2ef135cab9b8d2e2b6f
Lan Nagashima <Lan@Lans-MacBook-Pro.local>
2021-08-04 18:31:56 -0700

    completed

--- dijkstra.java
+++ dijkstra.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class dijkstra{
     static int [] track;
     static String [] airports = {"Haneda","San Francisco","Hong Kong","Rio de janeiro","Osaka","Nairobi","Washington D.C"};
+    static int [] [] listArr;
     public static class Edge{
         private int from;
         private int weight;
@@ -30,9 +31,9 @@ public class dijkstra{
         LinkedList<Integer> lists = new LinkedList<Integer>();
         IndexPQueue ipq = new IndexPQueue();
         int prev;
-        ipq.setUp(30);
-        ipq.push(start,0);
-        track[start]=0;
+        ipq.setUp(30);//create index priority queue
+        ipq.push(start,0);//push the starting point
+        track[start]=0;//record the distance(weight)
         while(ipq.getSize()!=0){
             int []arr = ipq.poll();//get key and value with minimum value
             prev = arr[0];
@@ -40,8 +41,15 @@ public class dijkstra{
             while(iterator.hasNext()){
                 Edge next = iterator.next();
             if(track[next.getTo()]>(track[prev]+next.getWeight())){
-                    track[next.getTo()]=track[prev]+next.getWeight();        
+                    track[next.getTo()]=track[prev]+next.getWeight();       
                     ipq.push(next.getTo(),track[next.getTo()]);
+                    listArr[next.getTo()][listArr.length]=0;//reset the size
+                    for(int i=0;i<listArr[prev][listArr.length];i++){
+                        listArr[next.getTo()][i] = listArr[prev][i];
+                        listArr[next.getTo()][listArr.length]++;
+                    }
+                    listArr[next.getTo()][listArr[next.getTo()][listArr.length]]= prev;
+                    listArr[next.getTo()][listArr.length]++;
                 }}
         }
         return lists;
@@ -51,8 +59,17 @@ public class dijkstra{
         Map<Integer, List<Edge>> graph = new HashMap<>();
         track = new int[airports.length];
         int start = 0;
+        listArr = new int[airports.length][];//keep tracks of what node was used to reach the current node
         for (int i = 0; i < airports.length; i++) graph.put(i, new LinkedList<Edge>());
         for(int i = 0; i<airports.length;i++)track[i]=Integer.MAX_VALUE;
+        for(int i=0; i<airports.length;i++){
+            listArr[i] = new int [airports.length+1];//the last digit will tell how many numbers are stored
+            for(int j=0;j<airports.length;j++){
+                listArr[i][j]=-1;
+
+            }
+            listArr[i][airports.length]=0;
+        }
         graph.get(0).add(new Edge(0,5,1));
         graph.get(0).add(new Edge(0,6,5));
         graph.get(0).add(new Edge(0,4,2));
@@ -75,6 +92,14 @@ public class dijkstra{
         System.out.println("Start Point: "+ airports[start]+"\nShortest Distances: \n");
         for(int i = 0; i<track.length;i++){
             System.out.println(airports[i]+": "+track[i]);
+            if(listArr[i][listArr.length]>0){
+                System.out.print("Route: ");
+                for(int j = 0; j<listArr[i][listArr.length];j++){
+                    System.out.print(" "+airports[listArr[i][j]]+" to");
+                }
+                System.out.println(" "+airports[i]);
+            }
+            
         }
     }
 }
